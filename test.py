41a17089fb2422639ac82319b5ef409c42fdcd64
Hadi <haditedi@gmail.com>
2019-04-26 13:55:20 +0000

    changed r to s

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ async def get_cheval():
 	return r
 
 async def get_garden():
-	r=await asession.get("https://be.synxis.com/?_ga=2.85469977.1228540469.1556119709-1038100479.1551104847&adult=1&arrive=2019-05-05&chain=21125&child=0&currency=GBP&depart=2019-05-06&hotel=3662&level=hotel&locale=en-GB&rooms=1&sbe_ri=0")
-	await r.html.arender()
-	return r
+	s=await asession.get("https://be.synxis.com/?_ga=2.85469977.1228540469.1556119709-1038100479.1551104847&adult=1&arrive=2019-05-05&chain=21125&child=0&currency=GBP&depart=2019-05-06&hotel=3662&level=hotel&locale=en-GB&rooms=1&sbe_ri=0")
+	await s.html.arender()
+	return s
 
 asession.run(get_cheval, get_garden)
 onebed=s.html.find("div[data-rate-code='AP1'] span",first=True).text
